dd49c46d81a97c1d0a3f5df25040e03b87acf6c2
Gabriel Gimenez <gabrielgimenezmoreira@gmail.com>
2023-04-19 14:46:57 -0300

    :sparkles: first project and deploy vercel

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner () {
+    return (
+        <header className="banner">
+            <img 
+                src="/imagens/banner.png"
+                alt="O banner Principal do projeto"/>
+        </header>
+    )
+}
+
+export default Banner;
